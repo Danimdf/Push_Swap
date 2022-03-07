bee0cd5edf108550878f3451a63d92a1deea2408
Daniela Monteiro <danimdf360@gmail.com>
2022-03-07 15:17:35 -0300

    Reverse rotate stack b and rotate two stacks in the same time move OK

--- Sources/moviments/reverse_rotate.c
+++ Sources/moviments/reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 09:16:31 by dmonteir          #+#    #+#             */
-/*   Updated: 2022/03/07 15:13:56 by dmonteir         ###   ########.fr       */
+/*   Updated: 2022/03/07 15:16:58 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,22 @@ void	reverse_rotate_a(t_stack **stack)
 	if ((*stack)->stack_a)
 		rmv_back_and_add(&((*stack)->stack_a), &value_remove);
 	write(2, "rra\n", 4);
+}
+
+void	reverse_rotate_b(t_stack **stack)
+{
+	int	value_remove;
+
+	if ((*stack)->stack_b)
+		rmv_back_and_add(&((*stack)->stack_b), &value_remove);
+	write(2, "rrb\n", 4);
+}
+
+void	reverse_rotate_r(t_stack **stack)
+{
+	reverse_rotate_a(stack);
+	reverse_rotate_b(stack);
+	write(2, "rrb\n", 4);
 }
 
 void	rmv_back_and_add(t_node **stack_mv, int *value_move)
